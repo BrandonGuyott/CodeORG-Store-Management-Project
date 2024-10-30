716e94b2f2663ae777a6acd0ab6a4cacbca634f3
BrandonGuyott <brandon100743@student-schoolsofwestfield.org>
2024-10-29 20:35:48 -0400

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+  public void printMenu() {
+    System.out.println("\nHere are the items on the menu:");
+    System.out.println("1) Pizza\n2) Pasta\n3) Mozzarella Sticks\n4) Garlic Bread");
+    System.out.println("Please press 1, 2, 3, or 4 to choose a dish. Exit with 'stop'");
+  }
+}
